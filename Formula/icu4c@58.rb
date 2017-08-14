c27e5d72e94e2a02e00924608eb1bc0de7254dde
Mark Tareshawty <tarebyte@github.com>
2017-08-14 14:45:11 -0400

    Remove bottle block

--- Formula/icu4c@58.rb
+++ Formula/icu4c@58.rb
@@ -8,13 +8,6 @@ class Icu4cAT58 < Formula
   sha256 "2b0a4410153a9b20de0e20c7d8b66049a72aef244b53683d0d7521371683da0c"
   head "https://ssl.icu-project.org/repos/icu/trunk/icu4c/", :using => :svn
 
-  bottle do
-    cellar :any
-    sha256 "779eda1c03f44b54300b5e70143f6bd4866b6f5f582e4216124649539164b029" => :sierra
-    sha256 "f8267df67e76db7412baf967dd9ef6e8c625cd207707960a6a2a860e0c07f92b" => :el_capitan
-    sha256 "7ce8fa8fba5ba285bfb0278d2e73c01e8d2ff3115a5ad762255cacde6ffeecfb" => :yosemite
-  end
-
   keg_only :provided_by_osx, "macOS provides libicucore.dylib (but nothing else)"
 
   option :cxx11
